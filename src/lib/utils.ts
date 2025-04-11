106e9315b9cf75dda4a418d9056ad24d95849764
sidedishjob <private0102iwa@gmail.com>
2025-04-11 21:39:06 +0900

    refactor: 共通ユーティリティ関数を TypeScript に移行

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -3,6 +3,6 @@
  * @param  {...string} classes - 結合するクラス名
  * @returns {string} 結合されたクラス名
  */
-export function cn(...classes) {
-  return classes.filter(Boolean).join(" ");
-} 
+export function cn(...classes: (string | false | null | undefined)[]): string {
+	return classes.filter(Boolean).join(" ");
+}
